0356f9fbb510f3d6313ea2477f726de726d933a3
perfect-Bee <joonslee3@gmail.com>
2025-12-22 16:47:22 +0900

    Step03 : List<Product> Category로 옮기는 중(1)

---
+++ src/category/Category.java
@@ -0,0 +1,23 @@
+package category;
+
+import product.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Product 클래스를 관리함
+// CommerceSystem 자료 -> Category로 이동
+public class Category {
+    List<Product> products = new ArrayList<>();
+    public Category(){
+        products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
+        products.add(new Product("IPhone 16", 1350000, "Apple의 최신 스마트폰", 9));
+        products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 8));
+        products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 7));
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+}
--- src/utils/CommerceSystem.java
+++ src/utils/CommerceSystem.java
@@ -5,23 +5,15 @@ import java.util.List;
 import java.text.NumberFormat;
 
 import product.Product;
+import category.Category;
 
 public class CommerceSystem {
-    List<Product> products = new ArrayList<>();
-    // products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
-    // products.add(new Product("IPhone 16", 1350000, "Apple의 최신 스마트폰", 9));
-    // products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 8));
-    // products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 7));
-    // 생성자는 클래스 안에서 선언해야 함 <<< 주의! 아무생각 없이 옮기지 말기
-    public CommerceSystem(){
-        products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
-        products.add(new Product("IPhone 16", 1350000, "Apple의 최신 스마트폰", 9));
-        products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 8));
-        products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 7));
-    }
+    private Category category;
 
     // 시작화면 : 입력과 반복문(출력)
     public void start(){
+        List<Product> products = category.getProducts();
+
         System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
         // NumberFormat을 활용한 포맷 형식 선언
         NumberFormat nf = NumberFormat.getInstance();

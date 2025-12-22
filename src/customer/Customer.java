d17927d9dd1b2d2278ac5290e86e2ba14ea015fe
perfect-Bee <joonslee3@gmail.com>
2025-12-22 18:54:37 +0900

    Step03 : 고객 정보 관리 클래스

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -1,6 +1,27 @@
 package customer;
 
 public class Customer {
-    // 고객명, 이메일, 등급 : customerName, customerEmail, customerRank
+    private String customerName;    // 이름
+    private String customerEmail;   // 이메일
+    private String customerGrade;   // 등급 (예: 일반, VIP)
 
+    // 생성자
+    public Customer(String name, String email, String grade) {
+        customerName = name;
+        customerEmail = email;
+        customerGrade = grade;
+    }
+
+    // getter 메서드들
+    public String getName() {
+        return customerName;
+    }
+
+    public String getEmail() {
+        return customerEmail;
+    }
+
+    public String getGrade() {
+        return customerGrade;
+    }
 }
